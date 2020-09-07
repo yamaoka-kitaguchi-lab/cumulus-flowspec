490429770f43b3c0c07ccf5d4ed76ca9fcb20de0
mi2428 <mi2428782020@gmail.com>
2020-09-08 00:18:32 +0900

    develop: Tue 08 Sep 2020 12:18:32 AM JST
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- process/flowspec.py
+++ process/flowspec.py
@@ -69,12 +69,12 @@ class ACL(object):
             pass
 
     @staticmethod
-    def _expand_protocol_match(protocols):
+    def _expand_text_match(protocols):
         # ToDo: Support commas, dots, relational operators and logical operators
         return re.sub('[!<>=]', '', protocols)
 
     @staticmethod
-    def _expand_port_match(ports):
+    def _expand_number_match(ports):
         # ToDo: Support commas, dots, relational operators and logical operators
         return re.sub('[!<>=]', '', ports)
 
@@ -83,7 +83,7 @@ class ACL(object):
         # ToDo: Support commas, dots, relational operators and logical operators
         acl = "[{v}]\n-A {c} -i {i}".format(v="iptables" if is_ipv4 else "ip6tables", c=cls.chain, i=cls.interface)
         if 'protocol' in flow:
-            acl += ' -p {p}'.format(p=cls._expand_protocol_match(flow['protocol'][0]))
+            acl += ' -p {p}'.format(p=cls._expand_text_match(flow['protocol'][0]))
         if 'source-ipv4' in flow:
             acl += ' -s {s}'.format(s=flow['source-ipv4'][0])
         if 'destination-ipv4' in flow:
@@ -93,12 +93,12 @@ class ACL(object):
         if 'destination-ipv6' in flow:
             acl += ' -d {d}'.format(d=flow['destination-ipv6'][0])
         if 'source-port' in flow:
-            acl += ' --sport {s}'.format(s=cls._expand_port_match(flow['source-port'][0]))
+            acl += ' --sport {s}'.format(s=cls._expand_number_match(flow['source-port'][0]))
         if 'destination-port' in flow:
-            acl += ' --dport {d}'.format(d=cls._expand_port_match(flow['destination-port'][0]))
+            acl += ' --dport {d}'.format(d=cls._expand_number_match(flow['destination-port'][0]))
         if 'icmp-type' in flow:
             acl += ' --icmp{v}-type {t}'.format(v="" if is_ipv4 else "v6",
-                                                t=cls._expand_port_match(flow['icmp-type'][0]))
+                                                t=cls._expand_text_match(flow['icmp-type'][0]))
         acl += ' -j {t}\n'.format(t='DROP' if is_drop else 'ACCEPT')
         return acl
 
